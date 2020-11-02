8d4f094cf6b35d1bc9b84af9e9938775140affa3
Arseniy Dobaev <Crxxvm@gmail.com>
2020-11-02 21:13:29 +0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO Organization (version, name, FullName, inn, kpp, address) VALUES (0, 'Gazprom', 'OOO GAZPROM', 1234567, 12345678, 'ул.Боткина');
+
+INSERT INTO Organization (version, name, FullName, inn, kpp, address) VALUES (0, 'SBERBANK', 'PAO SBERBANK', 6543123, 23467721, 'ул.Вавилова');
+
+INSERT INTO Office (version, org_Id, name) VALUES(0, 1, 'GAZPROM Office 1');
+
+INSERT INTO Office (version, org_Id, name) VALUES(0, 1, 'GAZPROM Office 2');
+
+INSERT INTO Office (version, org_Id, name) VALUES(0, 2, 'SBERBANK Office 1');
+
+INSERT INTO Office (version, org_Id, name) VALUES(0, 2, 'SBERBANK Office 2');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 1, 'User 1');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 1, 'User 2');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 2, 'User 1');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 2, 'User 2');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 3, 'User 1');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 3, 'User 2');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 4, 'User 1');
+
+INSERT INTO User (version, office_Id, firstName) VALUES(0, 4, 'User 2');
